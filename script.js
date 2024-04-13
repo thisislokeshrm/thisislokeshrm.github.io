e3fabf059a2a7fa3db9e63db76ef1151234283f8
thisislokeshrm <113114297+thisislokeshrm@users.noreply.github.com>
2024-04-13 14:34:17 +0530

    Adding the Files to Git

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    var menu = document.querySelector('.menu');
+    menu.classList.toggle('active');
+    var hamburger = document.querySelector('.hamburger');
+    hamburger.classList.toggle('active');
+}
